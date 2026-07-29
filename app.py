b2f9cc1583b78e45b150cd093826db12ce852d53
dmdm01030 <dmdm01030@gmail.com>
2026-07-29 02:10:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from datetime import datetime
 
@@ -240,16 +239,6 @@ def change_password():
             return redirect(url_for("index"))
 
     return render_template("change_password.html")
-
-
-@app.route("/reset-admin-tanporè-x92k")
-def reset_admin_emergency():
-    admin = User.query.filter_by(role="admin").first()
-    if admin:
-        admin.set_password("changeme123")
-        db.session.commit()
-        return "Modpas admin reset a 'changeme123'. Ale konekte, epi chanje modpas la touswit apre."
-    return "Pa gen okenn kont admin nan baz done a."
 
 
 # ---------------------------------------------------------------------------
